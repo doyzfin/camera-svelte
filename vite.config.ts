3289fde8fb98249ce5c994e0209a6b0c7f1a00af
alfin.ramadhan <alfinramadhan97@gmail.com>
2025-05-11 15:00:48 +0700

    add camera https

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+import mkcert from 'vite-plugin-mkcert';
 
-export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+export default defineConfig(({ mode }) => {
+	// Check if HTTPS is needed, for example based on the mode
+	const isHttps = mode === 'https';
+
+	return {
+		plugins: [sveltekit(), isHttps && mkcert()].filter(Boolean),
+		test: {
+			include: ['src/**/*.{test,spec}.{js,ts}']
+		},
+		server: {
+			https: isHttps, // Enable HTTPS if mode is 'https'
+			proxy: {}
+		}
+	};
 });
